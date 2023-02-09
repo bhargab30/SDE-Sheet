de4bf51b44ce07e256cfb2ed4fe3154dee788ca5
Bhargab Sarmah <64011806+bhargab30@users.noreply.github.com>
2023-02-10 00:30:52 +0530

    Update firebase.js

--- sdesheet/src/firebase.js
+++ sdesheet/src/firebase.js
@@ -5,13 +5,7 @@ import 'firebase/compat/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDOopNnzq-hLxAY9aH7-bTRN79fKw78h4w",
-    authDomain: "sdesheet-3466e.firebaseapp.com",
-    projectId: "sdesheet-3466e",
-    storageBucket: "sdesheet-3466e.appspot.com",
-    messagingSenderId: "174543996815",
-    appId: "1:174543996815:web:2bde8ed47e8c8de38c6dfa",
-    measurementId: "G-WM240JKPDN"
+    //add from firebase
   };
 
 
